cad4e6004124ade2ccc2e9a9b15f31091882a013
Vlad Chepish <59143771+ChepishVlad@users.noreply.github.com>
2024-09-09 00:27:12 +0300

    added getting members list functions

---
+++ get_members.lua
@@ -0,0 +1,65 @@
+local addonName, addonTable = ...
+
+-- Thx to Yudo for idea
+local GetMembers = LibStub("AceAddon-3.0"):NewAddon("GetMembers", "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0")
+local GetMembersGUI = LibStub("AceGUI-3.0")
+
+function GetMembers:OnInitialize()
+    self:AddRaidMembersButtonToSocialFrame()
+end
+
+function GetMembers:AddRaidMembersButtonToSocialFrame()
+    if not RaidFrame then
+        print("Окно рейда не найдено.")
+        return
+    end
+
+    local raidMembersButton = CreateFrame("Button", "RaidMembersButton", RaidFrame, "UIPanelButtonTemplate")
+    raidMembersButton:SetText("Участники")
+    raidMembersButton:SetSize(70, 20)
+    raidMembersButton:SetPoint("TOPLEFT", RaidFrame, "TOPLEFT", 250, -12)
+    raidMembersButton:SetScript("OnClick", function()
+        self:ShowRaidMembersPopup()
+    end)
+end
+
+function GetMembers:ShowRaidMembersPopup()
+    if not IsInRaid() then
+        print("Вы не в рейде.")
+        return
+    end
+
+    local raidMembers = {}
+    for i = 1, GetNumGroupMembers() do
+        local name = GetRaidRosterInfo(i)
+        if name then
+            table.insert(raidMembers, name)
+        end
+    end
+
+    if #raidMembers > 0 then
+        local membersString = table.concat(raidMembers, "\n")
+
+        local popupFrame = GetMembersGUI:Create("Frame")
+        popupFrame:SetTitle("Участники рейда")
+        popupFrame:SetLayout("Fill")
+        popupFrame:SetWidth(300)
+        popupFrame:SetHeight(400)
+
+        local membersEditBox = GetMembersGUI:Create("MultiLineEditBox")
+        membersEditBox:SetFullWidth(true)
+        membersEditBox:SetFullHeight(true)
+        membersEditBox:SetText(membersString)
+        membersEditBox:DisableButton(true)
+
+        popupFrame:AddChild(membersEditBox)
+        popupFrame:SetCallback("OnClose", function(widget)
+            GetMembersGUI:Release(widget)
+        end)
+    else
+        print("В рейде нет участников.")
+    end
+end
+
+
+GetMembers:OnInitialize()
